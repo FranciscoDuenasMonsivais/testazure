3955015171c08b5ae84ebda7d11a61db8b8a3b67
FranciscoDuenasMonsivais <123764522+FranciscoDuenasMonsivais@users.noreply.github.com>
2023-02-19 11:11:36 -0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('cypress');
-
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  e2e: {
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-    },
+  reporter: 'junit',
+  reporterOptions:{
+  mochaFile:'results/test-output-[hash].xml',
+  toConsole: true,
+   }, e2e: {
+  setupNodeEvents(on, config) {
   },
-});
+  },
+  video: false});
